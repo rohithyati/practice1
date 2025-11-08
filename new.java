63f303f9328884680540bab94cf699550c37d39e
Rohit Hyati <rohithyati123@gmail.com>
2025-11-08 13:45:06 +0530

    java

---
+++ new.java
@@ -0,0 +1,13 @@
+class new3{
+    public static void main(String[] args) {
+        System.out.println("Rohit");
+       int a=3;
+       int b=55;
+      int sum=a+b;
+      System.out.println(a+b);
+    }
+
+}
+
+    
+
